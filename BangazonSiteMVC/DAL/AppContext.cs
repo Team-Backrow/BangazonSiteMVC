f9b9c2af5d28a8f0e521088b256824f95ee5521f
Andi DeVois <adevois@gmail.com>
2017-05-09 18:09:28 -0500

    Added AppContext and the DBSet for Products

---
+++ BangazonSiteMVC/DAL/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using BangazonSiteMVC.Models;
+
+namespace BangazonSiteMVC.DAL
+{
+    public class AppContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
