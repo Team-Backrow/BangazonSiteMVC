6292321aec6392c29da23c15d626cd13c1c6d329
Josh Kilgore <kilgore.josh@gmail.com>
2017-05-09 18:50:07 -0500

    add initial files

---
+++ BangazonSiteMVC/DAL/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BangazonSiteMVC.Controllers;
+
+namespace BangazonSiteMVC.DAL
+{
+    public class CustomerRepository :ICustomerRepository
+    {
+
+    }
+}
