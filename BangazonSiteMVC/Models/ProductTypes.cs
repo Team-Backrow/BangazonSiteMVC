3947ac385b9eb83ee39cce4b9e56029f12f9dad1
Jennifer Dutton <jduttondesign@gmail.com>
2017-05-10 18:33:02 -0500

    producttypes

---
+++ BangazonSiteMVC/Models/ProductTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BangazonSiteMVC.Models
+{
+    public class PaymentTypes
+    {
+        public int CustomerId { get; set; }
+
+        public string CardType { get; set; }
+    }
+}
